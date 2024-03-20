14820855ca2f9a2b5631c207821dc122c48a77b5
AmberdownMoor <tatianaprisyazhnyuk2@gmail.com>
2024-03-20 12:47:44 +0200

    Added tests

---
+++ service_ltzo_408875_jcod.jsx
@@ -0,0 +1,13 @@
+const randomNumber = getRandomNumber();
+true - grape
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+const getRandomElement = array => array[getRandomIndex(array)];
+49,40,91,17,16,3,0,24,52,84,85,49 / banana
+const multiply = (a, b) => a * b;
+const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+
+grape
+
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+const removeDuplicates = array => Array.from(new Set(array));
